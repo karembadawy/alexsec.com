d7ab17da94cfca4eae637089c16861c3f66f607c
oddcoder <ahmedsoliman@oddcoder.com>
2017-01-02 15:21:35 +0200

    Fixing some shit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function() {
-        var target_date = new Date("Aug 1, 2017").getTime();
+        var target_date = new Date("Feb 10, 2017").getTime();
          
         var days, hours, minutes, seconds, countdown = document.getElementById("countdown");
          
